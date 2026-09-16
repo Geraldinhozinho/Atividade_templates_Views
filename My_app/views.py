a9d29d189adb1b77cdd1a65dc0b3e4443a98b609
Geraldo Júnior <161983302+Geraldinhozinho@users.noreply.github.com>
2026-09-16 20:43:49 -0300

    Create de views e Urls

--- My_app/views.py
+++ My_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def Home(request):
+    return render(request, 'My_app/Home.html')
+
+def Portifolio(request):
+    return render(request, 'My_app/Portifolio.html')
